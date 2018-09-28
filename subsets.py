acf1d51e9c8f5e3296b95adcac30108efa04dbf8
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-28 15:29:42 -0500

    Defined function

--- subsets.py
+++ subsets.py
@@ -1,1 +1,9 @@
 print("Subsets!")
+
+# Function takes in three parameters:
+# data: the array containing a given set
+# subset: an array that will contain the subsets of data
+# i: the current index.
+# No return value, only prints all subsets
+def subsets(data, subset, i):
+	  
